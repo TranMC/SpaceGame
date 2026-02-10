e20346ac88fca956e76d593bb19198cd2c92498d
TranMC <tranmc2005@gmail.com>
2026-02-10 20:47:53 +0700

    feat: Update file

---
+++ Assets/DemoScripts/DemoEnemyHealth.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    private void OnTriggerEnter2D(Collider2D collision) => Die();
+
+    private void Die()
+    {
+        var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+        Destroy(explosion, 1);
+        Destroy(gameObject);
+    }
+}
