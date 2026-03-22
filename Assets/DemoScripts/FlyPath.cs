d0806a3b948c081b87466e6e06f626ed223b4f18
TranMC <tranmc2005@gmail.com>
2026-03-22 18:35:24 +0700

    feat: Demo part 8

--- Assets/DemoScripts/FlyPath.cs
+++ Assets/DemoScripts/FlyPath.cs
@@ -9,4 +9,16 @@ public class FlyPath : MonoBehaviour
     {
         waypoints = GetComponentsInChildren<Waypoint>();
     }
+    private void OnDrawGizmos()
+    {
+        if (waypoints == null ) return;
+
+        Gizmos.color = Color.green;
+
+        for (int i = 0; i < waypoints.Length - 1; i++)
+        {
+            Gizmos.DrawLine(waypoints[i].transform.position, waypoints[i + 1].transform.position);
+        }
+    }
+    public Vector3 this[int index] => waypoints[index].transform.position;
 }
